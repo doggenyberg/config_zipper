dc24dd2e09874c62c31734e57a13cb4a8e44b2f0
Douglas Nyberg <douglas.nyberg@outlook.com>
2023-03-08 00:55:44 +0100

    working on page switching animations

--- resources/script/script.js
+++ resources/script/script.js
@@ -2,9 +2,29 @@
 
 function switchPage(currPage, nextPage)
 {   
-    currPage.classList.add("animation-fade")
-    currPage.style.display = "none";
-    nextPage.style.display = "flex";
+    currPage.classList.add("animation-fade-out");
+    mainContainer.style.height = findHeight(nextPage); 
+
+    setTimeout(function() {
+        currPage.style.display = "none";
+        
+        nextPage.classList.add("animation-fade-in");
+        nextPage.style.display = "flex";
+        currPage.classList.remove("animation-fade-out");
+        nextPage.classList.remove("animation-fade-in");
+    }, 300);
+    page = nextPage;
+}
+
+function findHeight(page) {
+    switch (page) {
+        case pageHome:
+            return "634px";
+            break;
+        case pageImport:
+            return "381px";
+            break;
+    }
 }
 
 /* ! Import HTML elements */
@@ -17,5 +37,12 @@ const pageImport = document.getElementById("import-page");
 const navBtnHome = document.getElementById("nav-home");
 const navBtnImport = document.getElementById("nav-import");
 
+// Main container
+const mainContainer = document.getElementById("main-container");
+
+// Page switching
+let page = pageHome;
+
 /* ! Event listeners */
-navBtnImport.addEventListener("click", () => switchPage(pageHome, pageImport));
+navBtnImport.addEventListener("click", () => switchPage(page, pageImport));
+navBtnHome.addEventListener("click", () => switchPage(page, pageHome));
