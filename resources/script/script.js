abe69d66614889651f9b32ec93d99bb9cca11fbf
Douglas Nyberg <douglas.nyberg@tele2.com>
2023-03-08 11:51:47 +0100

    working on footer

--- resources/script/script.js
+++ resources/script/script.js
@@ -2,17 +2,31 @@
 
 function switchPage(currPage, nextPage)
 {   
+    // prevent "bug"
+    if (currPage == nextPage) {
+        return
+    }
+
+    // remove all the animation-classes from the pages
+    currPage.classList.remove("animation-fade-out");
+    nextPage.classList.remove("animation-fade-in");
+    nextPage.classList.remove("animation-fade-out");
+    currPage.classList.remove("animation-fade-in");
+
+    // fade out current page
     currPage.classList.add("animation-fade-out");
     mainContainer.style.height = findHeight(nextPage); 
 
+    // fade in next page
     setTimeout(function() {
         currPage.style.display = "none";
         
         nextPage.classList.add("animation-fade-in");
-        nextPage.style.display = "flex";
         currPage.classList.remove("animation-fade-out");
-        nextPage.classList.remove("animation-fade-in");
+        nextPage.style.display = "flex";
     }, 300);
+
+    // save value for current page
     page = nextPage;
 }
 
@@ -20,10 +34,10 @@ function findHeight(page) {
     switch (page) {
         case pageHome:
             return "634px";
-            break;
         case pageImport:
             return "381px";
-            break;
+        case pageUpload:
+            return "410px"
     }
 }
 
@@ -32,10 +46,13 @@ function findHeight(page) {
 // Pages
 const pageHome = document.getElementById("home-page");
 const pageImport = document.getElementById("import-page");
+const pageUpload = document.getElementById("upload-page");
 
 // Buttons
 const navBtnHome = document.getElementById("nav-home");
 const navBtnImport = document.getElementById("nav-import");
+const navBtnUpload = document.getElementById("nav-upload");
+const homeBtnNext = document.getElementById("home-next");
 
 // Main container
 const mainContainer = document.getElementById("main-container");
@@ -46,3 +63,5 @@ let page = pageHome;
 /* ! Event listeners */
 navBtnImport.addEventListener("click", () => switchPage(page, pageImport));
 navBtnHome.addEventListener("click", () => switchPage(page, pageHome));
+navBtnUpload.addEventListener("click", () => switchPage(page, pageUpload));
+homeBtnNext.addEventListener("click", () => switchPage(page, pageImport));
